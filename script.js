08a61b0001923281c8271d3d8cbfba37e45424b9
wsasouza <wsasouza@hotmail.com>
2021-09-26 04:24:04 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// hover: background
+let list = document.querySelectorAll('.sci li');
+let bg = document.querySelector('.socialMedia');
+
+list.forEach(elements => {
+  elements.addEventListener('mouseenter', function(event){
+    let color = event.target.getAttribute('data-color');
+    bg.style.backgroundColor = color;
+  })
+
+  elements.addEventListener('mouseleave', function(event){    
+    bg.style.backgroundColor = '#fff';
+  })
+})
